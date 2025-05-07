2ce0bcc71b4269a7a05b0cad2c9079774ee1d09b
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2025-05-07 12:33:28 +0300

    complete task 1.5

--- tasks/todo_app/index.ts
+++ tasks/todo_app/index.ts
@@ -1,1 +1,13 @@
-console.log("Server started in port " + process.env.PORT)
+import express from "express";
+import type { Request, Response } from "express";
+const app = express()
+const PORT: String | undefined = process.env.PORT
+app.use(express.json())
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Kurkkumopo")
+})
+
+app.listen(PORT, () => {
+    console.log("Server started in port " + process.env.PORT)
+})
